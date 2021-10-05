11f1f5984abcbb95824a970edfa58d76b2a21892
Emil <emilagamamedov@gmail.com>
2021-10-05 03:04:37 +0300

    fourth day b solution

--- contest/28970/b.py
+++ contest/28970/b.py
@@ -6,18 +6,17 @@ import sys
 
 candidates_count = {}
 while True:
-   candidate_choices: list = sys.stdin.readline().split()
+    candidate_choices: list = sys.stdin.readline().split()
 
-   if len(candidate_choices) == 0:
-      break
+    if len(candidate_choices) == 0:
+        break
 
-   candidate, choices = candidate_choices[0], int(candidate_choices[1])
-   if candidate not in candidates_count:
-      candidates_count[candidate] = 0
+    candidate, choices = candidate_choices[0], int(candidate_choices[1])
+    if candidate not in candidates_count:
+        candidates_count[candidate] = 0
 
-   candidates_count[candidate] += choices
+    candidates_count[candidate] += choices
 
 sorted_cands = sorted(candidates_count.keys())
 for candidate in sorted_cands:
-   print(f"{candidate} {candidates_count[candidate]}")
-
+    print(f"{candidate} {candidates_count[candidate]}")
