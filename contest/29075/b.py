e653ce4f7f2697da232cbcd6a8d83c3e83269b17
emiliskan <emilagamamedov@gmail.com>
2021-11-18 15:06:59 +0300

    solved b solution

--- contest/29075/b.py
+++ contest/29075/b.py
@@ -1,28 +1,34 @@
 n: int = int(input().strip())
 nums: list[int] = list(map(int, input().split()))
 
-pref_sums = [0] * n
-max_pref_sum_i = 0
-min_pref_sum_i = 0
 max_sum = nums[0]
+cur_sum = nums[0]
 
-for i in range(0, n):
-    pref_sums[i] = pref_sums[i - 1] + nums[i - 1]
+for i in range(len(nums)):
+    if cur_sum < 0:
+        cur_sum = nums[i]
+    else:
+        cur_sum += nums[i]
+
+    max_sum = max(cur_sum, max_sum)
 
 print(max_sum)
 
 """
+8
+-1 -2 3 4 5 1 -2 -3
+
+3
+-1 0 -3
+
+6
+-1 -2 -3 -4 -5 -6
+
+4
 5 4 -10 4
-5 9 -1  3
-i = 1
-
-
-9
 
 5
 -1 -2 4 5 -7
 0 0 4 9 0
 
-6
--1 -2 -3 -4 -5 -6
 """
