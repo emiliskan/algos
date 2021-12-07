e0995f21354bfa9ec46ac97c89b15d1e88eb2802
Emil <emilagamamedov@gmail.com>
2021-12-07 18:58:58 +0300

    update merge sort

--- other/sort_phones/sort.py
+++ other/sort_phones/sort.py
@@ -1,89 +1,136 @@
 import os
 import time
 
-CHUNK_SIZE = 100_000
+CHUNK_SIZE = 10000
 
 
-def write_chunk(phones_chunk, n):
-    sorted_phones_chunk = sorted(phones_chunk)
-    chunk_file_name = f'files/phones_chunk_{n}'
-    with open(chunk_file_name, 'w') as phones_chunk_file:
-        phones_chunk_file.writelines(sorted_phones_chunk)
+class BigFileSort:
+    chunk_items: list = []
+    chunk_files: list = []
+    chunk_num: int = 0
+    part_num: int = 0
 
-    return chunk_file_name
+    def __init__(self, file: str):
+        self.file = file
 
+    def create_chunks(self):
+        with open(self.file, 'r') as data_file:
+            for item in data_file:
+                self._write_part_item(item)
 
-def create_chunks():
-    chunk_files = []
+            if self.chunk_items:
+                self._write_part_to_file()
 
-    with open('phones.csv', 'r') as phones_file:
+    def sort_chunks(self):
+        i = 0
+        while True:
+            chunk_file_name = self._merge_sort(
+                file_1=self.chunk_files[i],
+                file_2=self.chunk_files[i + 1],
+            )
 
-        phones_chunk = []
-        n = 1
-        for phone in phones_file:
-            phones_chunk.append(phone)
+            self.chunk_files.append(chunk_file_name)
+            self.chunk_num += 1
 
-            if len(phones_chunk) >= CHUNK_SIZE:
-                chunk_file_name = write_chunk(phones_chunk, n)
-                phones_chunk.clear()
-                n += 1
-                chunk_files.append(chunk_file_name)
+            # Файлы можно удалить сразу, чтоб не занимать память
+            # os.remove(self.chunk_files[i])
+            # os.remove(self.chunk_files[i + 1])
 
-        if phones_chunk:
-            chunk_file_name = write_chunk(phones_chunk, n)
-            chunk_files.append(chunk_file_name)
+            # Если больше файлов нет, значит мы все собрали и последняя запись chunk_files - итоговый файл
+            i += 2
+            if len(self.chunk_files) < i + 2:
+                break
 
-        return chunk_files
+        return self.chunk_files[-1]
 
+    def _write_part_item(self, item):
+        """
+        Сбор батчей и запись файл при превышении CHUNK_SIZE.
+        """
+        self.chunk_items.append(item)
+        if len(self.chunk_items) == CHUNK_SIZE:
+            self._write_part_to_file()
 
-def sort_chunks(chunk_files):
-    i = 0
-    while True:
-        chunk_file_name = f'files/sorted_phones_{i}'
+    def _write_part_to_file(self):
+        """
+        Сортировка и запись в файл батча.
+        """
+        chunk_file_name = f'files/chunk_part_{self.part_num}'
+        with open(chunk_file_name, 'w') as chunk_file:
+            self.chunk_items = sorted(self.chunk_items)
+            chunk_file.writelines(self.chunk_items)
 
-        with open(chunk_files[i], 'r') as chunk_file_1, open(chunk_files[i + 1], 'r') as chunk_file_2:
-            with open(chunk_file_name, 'w') as sorted_file:
-                phone_1 = next(chunk_file_1)
-                phone_2 = next(chunk_file_2)
+        self.part_num += 1
+        self.chunk_files.append(chunk_file_name)
+        self.chunk_items.clear()
+
+    def _write_sorted_item(self, item):
+        """
+        Сбор батчей и запись файл при превышении CHUNK_SIZE.
+        """
+        if len(self.chunk_items) < CHUNK_SIZE:
+            self.chunk_items.append(item)
+            return
+
+        self._write_to_sorted_file()
+
+    def _write_to_sorted_file(self):
+        """
+        Запись в файл батча.
+        """
+        self.chunk_file.writelines(self.chunk_items)
+        self.chunk_items.clear()
+
+    def _merge_sort(self, file_1, file_2):
+        """
+        Сортировка слиянием двух файлов.
+        """
+        chunk_file_name = f'files/chunk_sort_{self.chunk_num}'
+        with open(file_1, 'r') as chunk_file_1, open(file_2, 'r') as chunk_file_2:
+            with open(chunk_file_name, 'w') as chunk_file:
+                self.chunk_file = chunk_file
+
+                item_1 = next(chunk_file_1)
+                item_2 = next(chunk_file_2)
                 while True:
-                    if phone_1 < phone_2:
+                    if item_1 < item_2:
                         try:
-                            sorted_file.write(phone_1)
-                            phone_1 = next(chunk_file_1)
+                            self._write_sorted_item(item_1)
+                            item_1 = next(chunk_file_1)
                         except StopIteration:
-                            sorted_file.write(phone_2)
+                            self._write_sorted_item(item_2)  # Остаток от иттерации в другой ветке
                             break
                     else:
                         try:
-                            sorted_file.write(phone_2)
-                            phone_2 = next(chunk_file_2)
+                            self._write_sorted_item(item_2)
+                            item_2 = next(chunk_file_2)
                         except StopIteration:
-                            sorted_file.write(phone_1)
+                            self._write_sorted_item(item_1)  # Остаток от иттерации в другой ветке
                             break
 
-                # if any phone was left
+                # Если остались номера
                 for phone in chunk_file_1:
-                    sorted_file.write(phone)
+                    self._write_sorted_item(phone)
 
                 for phone in chunk_file_2:
-                    sorted_file.write(phone)
+                    self._write_sorted_item(phone)
 
-        # we don't need this files more
-        os.remove(chunk_files[i])
-        os.remove(chunk_files[i + 1])
-
-        chunk_files.append(chunk_file_name)
-        i += 2
-        if len(chunk_files) < i + 2:
-            break
-
-    return chunk_files[-1]
+                self._write_to_sorted_file()
+        return chunk_file_name
 
 
 if __name__ == '__main__':
-    chunks = create_chunks()
-    start = time.time()
-    result_file = sort_chunks(chunks)
-    end = time.time()
-    print(result_file)
-    print(end - start)
+    # file_sort = BigFileSort('phones.csv')
+    file_sort = BigFileSort('phones_test.csv')
+
+    start_chunk = time.time()
+    file_sort.create_chunks()
+    end_chunk = time.time()
+    print(f'Chunk create time: {end_chunk - start_chunk}')
+
+    start_sort = time.time()
+    file_sort.sort_chunks()
+    end_sort = time.time()
+    print(f'Sort time: {end_sort - start_sort}')
+
+    print(f'Total: {end_sort - start_chunk}')
